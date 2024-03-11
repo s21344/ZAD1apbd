7e84b375629b7d48984d2039cff52de342d64f1c
s21344 <s21344@pjwstk.edu.pl>
2024-03-11 17:08:19 +0100

    Feature-Final

--- MyCapp/MyCapp/Program.cs
+++ MyCapp/MyCapp/Program.cs
@@ -11,11 +11,31 @@ class Program
         }
         return x / tab.Length;
     }
+    
+    static int FindMax(int[] tab)
+    {
+        int x=0;
+        
+        for (int i = 0; i < tab.Length; i++)
+        {
+            if (i == 0)
+                x = tab[i];
+            else
+            {
+                if (x < tab[i])
+                    x = tab[i];
+            }
+
+        }
+        return x;
+    } 
+    
     public static void Main(string[] args)
     {
         int[] tbl = { 1, 7, 2, 8, 12, 20, 60, 23, -19, -57, 12 };
         Console.WriteLine("Average = " +FindAvg(tbl));
-        
+        Console.WriteLine("Maximum = " +FindMax(tbl));
+
     }
 
 }
