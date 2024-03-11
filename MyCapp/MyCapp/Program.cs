69447092ff821e27937d9a0427333eeaa20e780f
s21344 <s21344@pjwstk.edu.pl>
2024-03-11 16:57:12 +0100

    Modyfikacja 1

--- MyCapp/MyCapp/Program.cs
+++ MyCapp/MyCapp/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] tbl = { 1, 7, 2, 8, 12, 20, 60, 23, -19, -57, 12 };
+        
+    }
+
+}
